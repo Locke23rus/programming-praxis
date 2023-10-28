7fc4651364d428e28f29acff3231948d7f066286
Kirill Nikitin <locke23rus@gmail.com>
2023-10-29 02:19:56 +0300

    Add tests for 001

--- 001.rb
+++ 001.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
-# (19 + 2.14) * (4.5 - 2 / 4.3) = 85.2974
-input = '19 2.14 + 4.5 2 4.3 / - *'
-stack = []
+def evaluate(input)
+  stack = []
 
-input.split(' ').each do |token|
-  if '+-*/'.include?(token)
-    right = stack.pop
-    left = stack.pop
-    expression = "#{left} #{token} #{right}"
-    stack << eval(expression)
-  else
-    stack << token
+  input.split(' ').each do |token|
+    if '+-*/'.include?(token)
+      right = stack.pop
+      left = stack.pop
+      expression = "#{left} #{token} #{right}"
+      stack << eval(expression)
+    else
+      stack << token
+    end
   end
+
+  return stack.last.round(4)
 end
 
-puts stack.last.round(4)
+puts evaluate('19 2.14 + 4.5 2 4.3 / - *')
---
+++ 001_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require_relative './001'
+
+class Test001 < Minitest::Test
+  def test_evaluate
+    assert_equal evaluate('19 2.14 + 4.5 2 4.3 / - *'), 85.2974
+  end
+end
